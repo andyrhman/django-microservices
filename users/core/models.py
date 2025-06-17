48ce61491d4a368ddf7e9858bd824e1729ad7f35
andyrhman <andyrhmann@gmail.com>
2025-06-17 19:57:23 +0800

    Added shop users microservice not yot finished

---
+++ users/core/models.py
@@ -0,0 +1,56 @@
+import uuid
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError("User must have an email")
+        if not username:
+            raise ValueError("User must have a username")
+        if not password:
+            raise ValueError("User must have a password")
+
+        user = self.model(email=self.normalize_email(email), username=username)
+        user.set_password(password)
+        user.is_admin = False
+        user.is_staff = False
+        user.is_ambassador = False
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None):
+        if not email:
+            raise ValueError("User must have an email")
+        if not username:
+            raise ValueError("User must have a username")
+        if not password:
+            raise ValueError("User must have a password")
+
+        user = self.model(email=self.normalize_email(email), username=username)
+        user.set_password(password)
+        user.is_admin = True
+        user.is_ambassador = False
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractUser):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    fullName = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=255, unique=True)
+    is_user = models.BooleanField(default=True)
+    is_verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    first_name = None
+    last_name = None
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
+
+    objects = UserManager()
